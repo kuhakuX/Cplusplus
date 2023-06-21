92ca4303251a3b0dfd80700f78e2b07ea1917dc8
kuhakuX <112263004+kuhakuX@users.noreply.github.com>
2023-06-21 12:39:53 +0700

    Add files via upload

---
+++ SquareROOTcal.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  cout << "Square root of 7 = ";
+   
+  // print the square root of 7 ???
+  cout << sqrt(7);
+
+
+  return 0;
+}
+
+// Output: Square root of 7 = ?
